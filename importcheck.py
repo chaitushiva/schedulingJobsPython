316aaec93fe1fd29b33c66b801d6096a9cbfeb90
Student <student@Students-MacBook-Pro.local>
2018-04-05 19:02:19 -0400

    update

---
+++ importcheck.py
@@ -0,0 +1,6 @@
+print ('Check if this is imported or not!!!')
+if __name__ == "__main__":
+    print ("This is from main module")
+else:
+    print (__name__)
+    print ("This is from imported module")
